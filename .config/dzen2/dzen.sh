5eedcb750d788bc954b53302c80a6f91957699ec
Margaretha <margarethasaputri144@gmail.com>
2021-05-08 20:00:39 +0700

    fixing...

--- .config/dzen2/dzen.sh
+++ .config/dzen2/dzen.sh
@@ -2,108 +2,114 @@
 
 sleep 1;
 
-orange="#fa946e"
-red="#FA5AA4"
-blue="#63C5EA"
-green="#2BE491"
-magenta="#CF8EF4"
-font="M+ 1mn:style=Bold:size=10"
-icon_dir="$HOME/.local/share/icons/bitmap/"
+# COLORS
+ORANGE="#FA946E"
+RED="#FA5AA4"
+BLUE="#63C5EA"
+GREEN="#2BE491"
+MAGENTA="#CF8EF4"
+CYAN="#89CCF7"
+BACKGROUND="#F9F9F9"
+FOREGROUND="#4C566A"
+
+
+FONT="M+ 1mn:style=Bold:size=10"
+ICON_DIR="$HOME/.local/share/icons/bitmap/"
 
 get_time() {
-	icon_time="${icon_dir}tile.xbm"
-	current_time="$(date +"  %R %p  ")"
-	echo "^bg($orange)$current_time"
+	ICON_TIME="${ICON_DIR}tile.xbm"
+	CURRENT_TIME="$(date +"  %R %p  ")"
+	echo "^bg($BLUE)^fg($BACKGROUND)$CURRENT_TIME"
 }
 
 get_music() {
-	music_play="^i(${icon_dir}play.xbm)"
-	music_pause="^i(${icon_dir}pause.xbm)"
-	music_status=$(mpc | sed -n '2p' | awk '{print $1}')
-	#[[ -z $(mpc current) ]] && echo "" || echo "$music_icon Playing: $(mpc current)"
+	MUSIC_PLAY="^i(${ICON_DIR}play.xbm)"
+	MUSIC_PAUSE="^i(${ICON_DIR}pause.xbm)"
+	MUSIC_STATUS=$(mpc | sed -n '2p' | awk '{print $1}')
+	
 	if [[ -z $(mpc current) ]]; then
 		echo ""
-	elif [ "$music_status" = "[paused]" ]; then
-		echo "$music_pause Paused: $(mpc current)"
+	elif [ "$MUSIC_STATUS" = "[paused]" ]; then
+		echo "^fg($ORANGE)$MUSIC_PAUSE ^fg()Paused: $(mpc current)"
 	else
-		echo "$music_play Playing: $(mpc current)"
+		echo "^fg($ORANGE)$MUSIC_PLAY ^fg()Playing: $(mpc current)"
 	fi
 }
 
 get_network_speed() {
-	interface="wlp0s26u1u1"
-	netUp_icon="^fg($red)^i(${icon_dir}net_up_03.xbm)^fg()"
-	netDown_icon="^fg($blue)^i(${icon_dir}net_down_03.xbm)^fg()"
-	receive1=$(cat /sys/class/net/$interface/statistics/rx_bytes)
-	transfer1=$(cat /sys/class/net/$interface/statistics/tx_bytes)
+	INTERFACE="wlp0s26u1u1"
+	NETUP_ICON="^fg($RED)^i(${ICON_DIR}net_up_03.xbm)^fg()"
+	NETDOWN_ICON="^fg($BLUE)^i(${ICON_DIR}net_down_03.xbm)^fg()"
+	RECEIVE_1=$(cat /sys/class/net/$INTERFACE/statistics/rx_bytes)
+	TRANSFER_1=$(cat /sys/class/net/$INTERFACE/statistics/tx_bytes)
 	sleep 1
 
-	receive2=$(cat /sys/class/net/$interface/statistics/rx_bytes)
-	transfer2=$(cat /sys/class/net/$interface/statistics/tx_bytes)
+	RECEIVE_2=$(cat /sys/class/net/$INTERFACE/statistics/rx_bytes)
+	TRANSFER_2=$(cat /sys/class/net/$INTERFACE/statistics/tx_bytes)
 
-	receiveByte=$(expr $receive2 - $receive1)
-	transByte=$(expr $transfer2 - $transfer1)
+	RECEIVE_BYTE=$(expr $RECEIVE_2 - $RECEIVE_1)
+	TRANSFER_BYTE=$(expr $TRANSFER_2 - $TRANSFER_1)
 
-	receiveKB=$(expr $receiveByte / 1024)
-	transKB=$(expr $transByte / 1024)
+	RECEIVE_KB=$(expr $RECEIVE_BYTE / 1024)
+	TRANSFER_KB=$(expr $TRANSFER_BYTE / 1024)
 
-	echo "$netUp_icon Up: $transKB kB/s    $netDown_icon Down: $receiveKB kB/s"
+	echo "$NETUP_ICON Up: $TRANSFER_KB kB/s    $NETDOWN_ICON Down: $RECEIVE_KB kB/s"
 }
 
 get_weather() {
-	api_key="0755c771fe75b0120ec32ae2e76d84e7"
-	city_id="7688114"
+	API_KEY="0755c771fe75b0120ec32ae2e76d84e7"
+	CITY_ID="1625812"
 
-	icon_sunny="^i(${icon_dir}sunny.xbm)"
-	icon_cloudy="^i(${icon_dir}cloudy.xbm)"
-	icon_rainy="^i(${icon_dir}rain.xbm)"
-	icon_thunder="^i(${icon_dir}thunder.xbm)"
-	icon_snow="^i(${icon_dir}show.xbm)"
-	icon_misc="${icon_dir}empty.xbm"
+	ICON_SUNNY="^i(${ICON_DIR}sunny.xbm)"
+	ICON_CLOUDY="^i(${ICON_DIR}cloudy.xbm)"
+	ICON_RAINY="^i(${ICON_DIR}rain.xbm)"
+	ICON_THUNDER="^i(${ICON_DIR}thunder.xbm)"
+	ICON_SNOW="^i(${ICON_DIR}show.xbm)"
+	ICON_MISC="${ICON_DIR}empty.xbm"
 
 	
-	weather_url="http://api.openweathermap.org/data/2.5/weather?id=${city_id}&appid=${api_key}&units=metric"
-	weather_info=$(wget -qO- $weather_url)
-	weather_main=$(echo $weather_info | grep -o -e '\"main\":\"[a-Z]*\"' | awk -F ':' '{print $2}' | tr -d '"')
-	weather_temp=$(echo $weather_info | grep -o -e '\"temp\":\-\?[0-9]*' | awk -F ':' '{print $2}' | tr -d '"')
+	WEATHER_URL="http://api.openweathermap.org/data/2.5/weather?id=${CITY_ID}&appid=${API_KEY}&units=metric"
+	WEATHER_INFO=$(wget -qO- $WEATHER_URL)
+	WEATHER_MAIN=$(echo $WEATHER_INFO | grep -o -e '\"main\":\"[a-Z]*\"' | awk -F ':' '{print $2}' | tr -d '"')
+	WEATHER_TEMP=$(echo $WEATHER_INFO | grep -o -e '\"temp\":\-\?[0-9]*' | awk -F ':' '{print $2}' | tr -d '"')
 
-	if [[ $weather_main = *Snow* ]]; then
-		echo "$icon_snow Snow ${weather_temp}˚C"
-	elif [[ $weather_main = *Rain* ]] || [[ $weather_main = *Drizzle* ]]; then
-		echo "$icon_rainy Rainy ${weather_temp}˚C"
-	elif [[ $weather_main = *Cloud* ]]; then
-		echo "$icon_cloudy Cloudy ${weather_temp}˚C"
-	elif [[ $weather_main = *Clear* ]]; then
-		echo "$icon_sunny Sunny ${weather_temp}˚C"
-	elif [[ $weather_main = *Fog* ]] || [[ $weather_main = *Mist* ]]; then
-		echo "$icon_cloudy Fog ${weather_temp}˚C"
+	if [[ $WEATHER_MAIN = *Snow* ]]; then
+		echo "^fg($CYAN)$ICON_SNOW ^fg()Snow ${WEATHER_TEMP}˚C"
+	elif [[ $WEATHER_MAIN = *Rain* ]] || [[ $WEATHER_MAIN = *Drizzle* ]]; then
+		echo "^fg($CYAN)$ICON_RAINY ^fg()Rainy ${WEATHER_TEMP}˚C"
+	elif [[ $WEATHER_MAIN = *Cloud* ]]; then
+		echo "^fg($CYAN)$ICON_CLOUDY ^fg()Cloudy ${WEATHER_TEMP}˚C"
+	elif [[ $WEATHER_MAIN = *Clear* ]]; then
+		echo "^fg($CYAN)$ICON_SUNNY ^fg()Sunny ${WEATHER_TEMP}˚C"
+	elif [[ $WEATHER_MAIN = *Fog* ]] || [[ $WEATHER_MAIN = *Mist* ]]; then
+		echo "^fg($CYAN)$ICON_CLOUDY ^fg()Fog ${WEATHER_TEMP}˚C"
 	else
-		echo "$icon_misc Unknown"
+		echo "^fg($CYAN)$ICON_MISC ^fg()Unknown"
 	fi
 }
 
 get_memory() {
-	icon_memory="${icon_dir}mem.xbm"
-	current_memory=$(printf "%.f" $(free | grep Mem | awk '{print $3/$2 * 100.0}'))
-	echo "^fg($magenta)^i($icon_memory)^fg() Memory: $current_memory% "
+	ICON_MEMORY="${ICON_DIR}mem.xbm"
+	CURRENT_MEMORY=$(printf "%.f" $(free | grep Mem | awk '{print $3/$2 * 100.0}'))
+	echo "^fg($MAGENTA)^i($ICON_MEMORY)^fg() Memory: $CURRENT_MEMORY% "
 }
 
 get_cpu() {
-	cpu_icon="${icon_dir}cpu.xbm"
+	CPU_ICON="${ICON_DIR}cpu.xbm"
 	read cpu user nice system idle iowait irq softirq steal guest < /proc/stat
-	cpu_active_prev=$((user+system+nice+softirq+steal))
-	cpu_total_prev=$((user+system+nice+softirq+steal+idle+iowait))
+	CPU_ACTIVE_PREV=$((user+system+nice+softirq+steal))
+	CPU_TOTAL_PREV=$((user+system+nice+softirq+steal+idle+iowait))
 
 	usleep 50000
 
 	read cpu user nice system idle iowait irq softirq steal guest < /proc/stat
-	cpu_active_cur=$((user+system+nice+softirq+steal))
-	cpu_total_cur=$((user+system+nice+softirq+steal+idle+iowait))
-	cpu_util=$((100 * (cpu_active_cur - cpu_active_prev) / (cpu_total_cur - cpu_total_prev)))
-	echo "^fg($green)^i($cpu_icon)^fg() $(printf "CPU: %s%%" $cpu_util)"
+	CPU_ACTIVE_CUR=$((user+system+nice+softirq+steal))
+	CPU_TOTAL_CUR=$((user+system+nice+softirq+steal+idle+iowait))
+	CPU_UTIL=$((100 * (CPU_ACTIVE_CUR - CPU_ACTIVE_PREV) / (CPU_TOTAL_CUR - CPU_TOTAL_PREV)))
+	echo "^fg($GREEN)^i($CPU_ICON)^fg() $(printf "CPU: %s%%" $CPU_UTIL)"
 }
 
 while true; do
-	echo "^fn($font)$(get_music)    $(get_network_speed)    $(get_cpu)    $(get_memory)   $(get_weather)    $(get_time)";
+	echo "^fn($FONT)$(get_music)    $(get_network_speed)    $(get_cpu)    $(get_memory)   $(get_weather)    $(get_time)";
 	sleep 1;
-done | dzen2 -x 410 -y 10 -h 24 -w 940 -p -ta r -e ''
+done | dzen2 -fg $FOREGROUND -bg $BACKGROUND -x 410 -y 10 -h 24 -w 940 -p -ta r -e ''
